9213e6b71396d82417b7dd81580afc32ace7566b
mullummer <azijlstra@gmail.com>
2021-09-25 14:59:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // run script in javascript console on the live data page (https://racecenter.lavuelta.es/en)
 // code by @velofacts
-var viplist = ',1,2,3,4,'; 
+var viplist = ',1,2,3,4,5,6,7,8,53,74,29,19,23,9,15,66,37,10,11,45,22,34,125,16,'; 
 var mycolor = '#ff4500';
+var html = '';
 
 select_team = function() {
     var jersey = document.getElementsByClassName("teamJersey");
@@ -28,19 +29,20 @@ toggle_pause = function() {
     return false;
 };
 
-
+// css
+$('body').css('background','none');
 var style = document.createElement('style');
 style.innerHTML = '.group {border-bottom: 2px solid #ba4a19; margin-bottom: 3px; font-size: 11px; color: #333} .gc div { background-color: #ff0; border: 1px solid #ffb700; } .green div { background-color: #45AE51; color: #fff}  .mylist div { background-color: '+ mycolor +'; color: #fff}  .slow div {color: #fff; background-color: #000} .team {color:#fff; background-color: #ba4a19; } #q-app {padding: 10px} #pause {font-size: 48px;} .group div div { padding-left: 2px; padding-right: 2px;} #toolbar { display: flex; background: #fee5d9; padding: 10px; border-bottom: 2px solid #ba4a19; align-items: center } #toolbar > img { width: 72px; } #jerseyWrapper { display: flex;margin: auto; align-items: center; } .row { justify-content: center; } .selected { background: #ffa47b; } .fas { color: #ba4a19; } .row>.col-md-2 { padding: 2px 5px } .teamJersey:hover { background: #ffa47b; } #toolbar .distance { padding-left: 10px; font-size: 24px } #jerseyWrapper a {width: 3.9%} #jerseyWrapper img {max-width:100%}';
+style.innerHTML += '.info {color: #fff; background-color: #ba4a19; width: 200px; float: right; margin-top: -5px}';
 document.head.appendChild(style);
-
 var link = document.createElement('link');
 link.rel = 'stylesheet';
-link.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css';
+link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css';
 document.head.appendChild(link);
 
 //https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css
 // html
-document.getElementsByClassName("container")[0].innerHTML = '<div id="toolbar"><a href="#" id="pause"><i id="pause-icon" class="fas fa-pause-circle">pause</i></a><span class="distance"><span id="distance">...</span> km</span><div id="jerseyWrapper"></div></div><div id="rows">&nbsp; waiting for data...</div>';
+$('body').html('<div id="toolbar"><a href="#" id="pause"><i id="pause-icon" class="fas fa-pause-circle"></i></a><span class="distance"><span id="distance">...</span> km</span><div id="jerseyWrapper"></div></div><div id="rows">&nbsp; waiting for data...</div>');
 
 // team jerseys
 /*
@@ -66,22 +68,26 @@ for (var i = 0; i < teams.length; i++) {
             if (((n = unCompressString(n)), (t = unCompressString(t)), (i = unCompressString(i)))) {
                 r = 0;
                 f = JSON.parse(i);
-                console.log(f);
+                // console.log(f);
 
 
-                var html = '<div class="row group">';
+                html = '<div class="row group">';
 
                 for (var index = 0; index < f.length; index++) {
 		    group = f[index];
-                    if (group.DistanceFromStart != -1) {
-                      html += '</div><div class="row group">';
-                      html += group.HTML;
-                      $(group.HTML).find('.gtoupsInfos tr').each(function () {
+			if(index == 0) {$('#distance').html(group.DistanceToFinish)}
+			if (group.DistanceFromStart != -1) {
+				html += '</div>';
+				var speed = $(group.HTML).find('.speed').text();
+			    if(index >= 0) {html += '<div class="info">'+group.DistanceToFinish+'km ' + speed +'km/h</div><div>&nbsp;</div>'}
+			    html += '<div class="row group">';
+			    $(group.HTML).find('tr').each(function () {
 			      var bib = $(this).find('.bib').text();
 			      var extra_class = "";
+			      if (viplist.includes(',' + bib + ',')) { extra_class += ' gc'}
 			      var name = $(this).find('.name').text()
-                              html += '<div class="col-md-2 ' + extra_class + '"><div>' + name + '</div></div>';
-		      }) 
+                              html += '<div class="col-2 ' + extra_class + '"><div>' + name + '</div></div>';
+			    }) 
 			    
 			    
                       document.getElementById("rows").innerHTML = html + '</div>';
@@ -93,70 +99,3 @@ for (var i = 0; i < teams.length; i++) {
 	    }
 
         });
-
-/*
-
-
-
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        tmp = JSON.parse(this.responseText);
-        for (var i = 0; i < tmp.length; i++) {
-            peloton[tmp[i].bib] = tmp[i]
-        }
-    }
-    start_listening();
-};
-
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-
-function start_listening() {
-    velofacts.addEventListener("update", function(e) {
-        if (!paused) {
-            var d = JSON.parse(e.data);
-            var html = '<div class="row group">';
-            var previous_gap = 0;
-            if (d.bind == 'telemetryCompetitor-2021') { //was 
-                var riders = d.data.Riders;
-                for (var i = 0; i < riders.length; i++) {
-                    var rider = riders[i];
-                    if (i == 0) { document.getElementById("distance").innerHTML = rider.kmToFinish}
-                    var gap = rider.secToFirstRider;
-                    var speed = rider.kph;
-                    var speedAvg = rider.kphAvg;
-                    var extra_class = '';
-                    if ((gap - previous_gap) > min_gap) {
-                        html += '</div><div class="row group">';
-                    }
-                    if (selected_team == '') {
-                        // show points and gc VIP's
-                        if (gc.includes(',' + rider.Bib + ',')) {
-                            extra_class += ' gc'
-                        }
-                        if (green.includes(',' + rider.Bib + ',')) {
-                            extra_class += ' green'
-                        }
-			if (mylist.includes(',' + rider.Bib + ',')) {
-                            extra_class += ' mylist'
-                        }
-                    } else {
-                        //
-                        if (peloton[rider.Bib].$team.split(':')[1] == selected_team) extra_class += ' team';
-                    }
-                    if (speed < max_slow_speed) extra_class = 'slow';
-                    html += '<div title="Speed: ' + speed + 'km/h | Average Speed: ' + speedAvg + 'km/h | ' + rider.kmToFinish + 'km to go" class="col-md-2 ' + extra_class + '"><div>' + peloton[rider.Bib].lastnameshort + ' ' + peloton[rider.Bib].firstname + ' ' + prety_time(gap) + '</div></div>';
-                    if (gap > 0) previous_gap = gap;
-                }
-                document.getElementById("rows").innerHTML = html + '</div>';
-                riders_prev = riders;
-            } else {}
-        }
-    });
-}
-
-
-
-*/
